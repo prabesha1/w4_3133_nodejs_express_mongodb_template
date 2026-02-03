b4e2922ea7e3fe1585945ae44b833f7cb5822661
moxDroid Labs Inc <pritam.world@gmail.com>
2026-02-03 18:02:41 -0500

    code upgrade

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const employeeRouter = require('./routes/EmployeeRoutes.js');
+const SERVER_PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json()); // Make sure it comes back as json
 
 //TODO - Replace you Connection String here
-mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(success => {
+
+//TODO - Replace you Connection String here
+const DB_NAME = "db_comp3133_employee"
+const DB_USER_NAME = ''
+const DB_PASSWORD = ''
+const CLUSTER_ID = ''
+const DB_CONNECTION = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.${CLUSTER_ID}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.connect(DB_CONNECTION).then(success => {
   console.log('Success Mongodb connection')
 }).catch(err => {
   console.log('Error Mongodb connection')
@@ -17,4 +23,4 @@ mongoose.connect('PASTE_YOUR_CONNECTION_STRING_HERE', {
 
 app.use(employeeRouter);
 
-app.listen(8081, () => { console.log('Server is running...') });
+app.listen(SERVER_PORT, () => { console.log('Server is running...') });
